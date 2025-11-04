44b543f614bf853c41096191fb92e2aab40e9515
Jeremy Smith <jeremy.ws.2008@gmail.com>
2025-11-04 14:37:53 -0800

    Sync commit

--- cmmunities_host/src/tree/tree.h
+++ cmmunities_host/src/tree/tree.h
@@ -21,26 +21,51 @@ The header of the file contains:
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define TREE_HEADER_SIZE ;
+#include <errno.h>
+
+#define TREE_HEADER_SIZE 8;
 #define ENTRY_SIZE 36
 
-Tree open_tree(char* name){
-  int underlying_fd;
-  char buffer[36];
-  ssize_t bytes_read;
-  
-  int fd = open(name, O_RDWR | O_EXCL | O_CREAT);
-  
+int open_tree(char* name, Tree* surrogate){
+  int fd;
+  off_t offset = 0;
+
+  uint64_t len, end_of_entries, free_indexes_start;
+  fd = open(name, O_RDWR | O_EXCL | O_CREAT);
+  if (fd != -1) {
+    create_tree(fd, 10);
+  }
+  else if (errno != EEXIST) return -1; //Error Handling needed
+
+  if (lseek(fd, offset, SEEK_SET) == -1) return -1; //Error Handling needed
+  if (read(fd, &len, 8) <= 0) return -1; //Error Handling needed
+  offset += 8;
+  if (lseek(fd, offset, SEEK_SET) == -1) return -1;
+  if (read(fd, &end_of_entries, 8) == -1) return -1;
+  offset += 8;
+  if (lseek(fd, offset, SEEK_SET) == -1) return -1;
+  if (read(fd, &free_indexes_start, 8) == -1) return -1;
+  *surrogate = (Tree){
+    .underlying_fd = fd,
+    .len = len,
+    .end_of_entries = end_of_entries,
+    .free_indexes_start = free_indexes_start,
+  };
+  return 0;
+}
+//ONLY CALLED if the file was JUST opened
+static void create_tree(int fd, size_t free_index_offset){
   
 }
 
 typedef struct Tree{
   int underlying_fd;
-  size_t len;
-  size_t end_of_entries;
-  size_t free_indexes_start;
+  uint64_t len;
+  uint64_t end_of_entries;
+  uint64_t free_indexes_start;
   
 } Tree;
